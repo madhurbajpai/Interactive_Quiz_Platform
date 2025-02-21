10e68c1567d7cda4a603f047f943288479b7bfbf
madhurbajpai <mdhrbajpai@gmail.com>
2025-02-22 01:01:43 +0530

    modified entry page

--- frontend/src/components/EntryPage.jsx
+++ frontend/src/components/EntryPage.jsx
@@ -3,45 +3,25 @@ import { useNavigate } from "react-router-dom";
 import "./EntryPage.css";
 
 const EntryPage = () => {
-  const [email, setEmail] = useState("");
   const navigate = useNavigate();
 
-  const validateEmail = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-
   const handleStartQuiz = () => {
-    if (validateEmail(email)) {
-      const userEmail = localStorage.getItem("userEmail");
-      if (email === userEmail) {
-        navigate("/analyze"); // navigate to analyze page
-      } else {
-        localStorage.setItem("userEmail", email); // Save email for tracking
-        navigate("/quiz"); // navigate to quiz page
-      }
-    } else {
-      alert("Please enter a valid email address.");
-    }
+    
+    navigate("/quiz"); // navigate to quiz page
   };
 
   const handleAnalyze = () => {
-    const userEmail = localStorage.getItem("userEmail");
-    if (email === userEmail) {
-      navigate("/analyze"); // navigate to analyze page
-    } else {
+    const quiz = localStorage.getItem("quizHistory");
+    if (!quiz) {
       alert("You need to attempt a quiz first.");
+    } else {
+      navigate("/analyze"); // navigate to analyze page
     }
   };
 
   return (
     <div className="entry-page">
-      <h2 className="heading">Enter Your Email to Start the Quiz</h2>
-
-      <input
-        className="input"
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Enter your email"
-      />
+      <h2 className="heading">Interactive Quiz Platform</h2>
       <div className="btn-holder">
         <button onClick={handleStartQuiz}>Start Quiz</button>
         <button onClick={handleAnalyze}>Analyze</button>
